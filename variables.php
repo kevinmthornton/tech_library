a4e51e893cb3b700a6ad0e957ef2f0bded8329f5
kevinmthornton <kevint@clear.net>
2014-03-21 08:38:23 -0700

    Update variables.php

--- variables.php
+++ variables.php
@@ -2,7 +2,7 @@
 # this should be kept above the /var/www/html directory just in case
 $SQL_DB_SERVER         = "localhost"; 
 $SQL_DB_NAME     = "tech_library"; 
-$SQL_USERNAME  = "library_user";
-$SQL_PASSWORD = "uuHHTRee45vrT";
+$SQL_USERNAME  = "NNNN";
+$SQL_PASSWORD = "NNNN";
 
 ?>
